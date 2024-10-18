217c490f48838d5e3b56090cb8ff7a5a2ad0731e
Nitish Singh <nitishsingh925@gmail.com>
2024-10-18 21:46:47 +0530

    base with node and bun both

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("expresskey");
+import { app } from "./app.js";
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello World !" });
+});
+
+process.loadEnvFile();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
