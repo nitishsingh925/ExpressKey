036dad44680572465385278456598b9ca1ce1fc4
Nitish Singh <nitishsingh925@gmail.com>
2025-05-04 01:00:00 +0000

    update

--- src/model/auth.model.js
+++ src/model/auth.model.js
@@ -1,9 +1,11 @@
 // auth model
+
 import { model, Schema } from "mongoose";
 const authSchema = new Schema({
   email: { type: String },
   password: { type: String },
   sessionId: { type: String },
 });
+
 const Auth = model("Auth", authSchema);
 export { Auth };
